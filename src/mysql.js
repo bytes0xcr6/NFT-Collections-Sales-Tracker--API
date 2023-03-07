d64a0b13c7935ca60e199db378a9dfddda3409b4
cristianRicharte6 <cristianricharte6@gmail.com>
2023-03-07 10:00:32 +0100

    Update

--- src/mysql.js
+++ src/mysql.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connector = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "691718775",
-  database: "",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
 });
