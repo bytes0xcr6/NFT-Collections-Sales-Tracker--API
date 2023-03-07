426e9b485c08eddd176116e989e1c68da6a64b0a
cristianRicharte6 <cristianricharte6@gmail.com>
2023-03-07 10:20:18 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const { connector } = require("./mysql.js");
 
 app.get("/", (req, res) => {
   res.send("Welcome to my Server!");
 });
 
+app.get("/api/users", (req, res) => {
+  connector.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${PORT}`);
 });
+
+// console.log(connector);
--- src/mysql.js
+++ src/mysql.js
@@ -3,7 +3,13 @@ const mysql = require("mysql2");
 
 const connector = mysql.createPool({
   host: process.env.HOST,
+  port: process.env.PORT_DB,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DB,
 });
+
+connector.execute("SELECT * FROM users", (err, result) => {
+  console.log(typeof result);
+});
+module.exports = { connector };
